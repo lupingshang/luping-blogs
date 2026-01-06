8a783027d0192219dbbf064f66a505c03720ef12
lupingshang <18061449589@163.com>
2026-01-06 17:55:12 +0800

    1

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+interface User {
+  name: string;
+  age: number;
+  info?: number;
+}
 export default function HomeDashboardPage() {
+  useEffect(() => {
+    type UserKeys = keyof User;
+    let xiaoming: UserKeys = "name";
+    type MyPartial = {
+      [key in keyof User]?: string;
+    };
+    type GetReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+
+    // 测试
+    type MyFunc = (x: number) => string;
+    type Result = GetReturnType<MyFunc>; // ✅ 结果是 `string`！
+  }, []);
   return <div>主页</div>;
 }
